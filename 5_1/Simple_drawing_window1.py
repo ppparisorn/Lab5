a274a1e4c8891facfa7d5563c94a7d6532bcd5ae
ppparisorn <148557409+ppparisorn@users.noreply.github.com>
2023-12-28 13:32:07 +0700

    create

---
+++ 5_1/Simple_drawing_window1.py
@@ -0,0 +1,17 @@
+import sys
+from PySide6.QtCore import *
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
+
+class Simple_drawing_window1(QWidget):
+    def __init__(self):
+        QWidget.__init__(self , None)
+        self.setWindowTitle("Simple Github Drawing")
+        
+    def paintEvent(self , e):
+        p = QPainter()
+        p.begin(self)
+        
+        p.setPen(QColor(0 , 0 , 0))
+        p.setBrush(QColor(0 , 127 , 0))
+        p.drawPolygon([QPoint(70 , 100) , QPoint(20 , 200) , QPoint() , QPoint() , QPoint()])
