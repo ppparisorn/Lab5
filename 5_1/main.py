983b3d47309511f24fffe316050d7527847b2972
Soe Moe Htet-Stephen <mobilesoemoehtet11@gmail.com>
2023-12-28 13:56:37 +0700

    main py file updated

--- 5_1/main.py
+++ 5_1/main.py
@@ -1,3 +1,4 @@
+from Simple_drawing_window import Simple_drawing_window
 from Simple_drawing_window1 import Simple_drawing_window1
 from Simple_drawing_window2 import Simple_drawing_window2
 
@@ -6,15 +7,21 @@ from PySide6.QtWidgets import *
 import sys
 
 def main():
-    app = QApplication(sys.argv)
-    
-    w1 = Simple_drawing_window1()
-    w1.show()
-    
-    w2 = Simple_drawing_window2()
-    w2.show()
-    
-    app.exec()
+      app = QApplication(sys.argv)
+      
+      w = Simple_drawing_window()
+
+      w.show()
+
+      w1 = Simple_drawing_window1()
+      w1.show()
+      
+      w2 = Simple_drawing_window2()
+      w2.show()
+
+      
+      
+      app.exec()
 
 if __name__ == "__main__":
       sys.exit(main())
