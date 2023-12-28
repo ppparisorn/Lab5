256b2f525c6d7ed71e81ff335a146e5ce68da27f
ppparisorn <148557409+ppparisorn@users.noreply.github.com>
2023-12-28 13:42:47 +0700

    some of the thing

--- 5_1/Simple_drawing_window.py
+++ 5_1/Simple_drawing_window.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window1(QWidget):
+class Simple_drawing_window(QWidget):
       def __init__(self):
           QWidget.__init__(self , None)
           self.setWindowTitle("Simple Github Drawing")
